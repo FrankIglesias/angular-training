5e35fdc62e3a406915dfe88790f6a0e79994e8fb
FrankIglesias <franciscoj.iglesias@hotmail.com>
2018-07-07 13:53:27 -0300

    Added folder structure and services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { HttpClient } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
+import { LoginComponent } from './screens/login/login.component';
 
 @NgModule({
   declarations: [
